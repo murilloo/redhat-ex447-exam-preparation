af8c4d15ba68b27c5180d216e0faf52aa9475cb1
Murillo Souza <murilloo@gmail.com>
2022-08-18 18:34:40 +0200

    Fixed user pass

--- round1/12-curl_launch_tower_job.sh
+++ round1/12-curl_launch_tower_job.sh
@@ -8,7 +8,7 @@ pass=password
 sudo yum install jq -y
 
 # Get Job ID based on the name
-id=`curl -X GET --user admin:password https://tower.example.com/api/v2/workflow_job_templates/ -ks | jq '.results | .[] | select(.name=="your-workflow") | .id'`
+id=`curl -X GET --user $user:$pass https://tower.example.com/api/v2/workflow_job_templates/ -ks | jq '.results | .[] | select(.name=="your-workflow") | .id'`
 
 # Calling API
 curl -X POST --user $user:$pass https://tower.example.com/api/v2/workflow_job_templates/$id/launch/ -ks 2&> /dev/null
